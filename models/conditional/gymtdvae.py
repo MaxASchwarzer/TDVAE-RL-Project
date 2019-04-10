462840d6ee332011ce8609db82aa0062d71cdd08
Ankit Vani <a@nevitus.org>
2019-04-10 16:33:48 -0400

    Remove redundant operation

--- models/conditional/gymtdvae.py
+++ models/conditional/gymtdvae.py
@@ -532,8 +532,8 @@ class GymTDQVAE(BaseGymTDVAE):
             target_q2 = r2_next + self.flags.discount_factor * q2_next
 
             # TODO remove tdvae weighing when we have b->z path for q1
-            rl_loss = (self.flags.tdvae_weight * F.smooth_l1_loss(pred_q1, target_q1) +
-                       F.smooth_l1_loss(pred_q2, target_q2)).mean()
+            rl_loss = self.flags.tdvae_weight * F.smooth_l1_loss(pred_q1, target_q1, reduction='mean') + \
+                F.smooth_l1_loss(pred_q2, target_q2, reduction='mean')
         else:
             rl_loss = 0.0
 
