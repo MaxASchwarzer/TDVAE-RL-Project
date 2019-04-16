854948189d6297d02ddc92e6cca241c4f2fe39c2
Ankit Vani <a@nevitus.org>
2019-04-16 10:26:34 -0400

    increase max_save_files to 2 again

--- readers/gym_reader.py
+++ readers/gym_reader.py
@@ -173,9 +173,9 @@ class ReplayBuffer(Reader):
                 # first, get segments that don't have dones within them (except for final state)
                 done_idx = np.nonzero(bool_done)[0]
                 min_t1s = np.concatenate([[-1], done_idx]) + 1
-                max_t2s = np.concatenate([done_idx, [truncated_len - 1]]) - 1 # t_len-1 to leave room for next reward
+                max_t2s = np.concatenate([done_idx, [truncated_len - 1]]) - 1  # -1 to leave room for next reward
                 max_t1s = max_t2s - t_diff_min
-                t1s_possible = max_t2s - min_t1s - t_diff_min + 1
+                t1s_possible = max_t1s - min_t1s + 1
                 if np.all(t1s_possible < 1):
                     continue  # skip this trajectory
                 possible_combs = np.zeros_like(t1s_possible)
