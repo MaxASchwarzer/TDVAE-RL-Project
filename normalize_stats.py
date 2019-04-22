a6add3b864757a1fd075d3182936b88a216f490b
Ankit Vani <a@nevitus.org>
2019-04-22 19:05:40 -0400

    Disable normalization

--- normalize_stats.py
+++ normalize_stats.py
@@ -43,23 +43,25 @@ if __name__ == '__main__':
     batches = get_batches(DATA_DIR + '/norm_batches.pk')
     misc.save_comparison_grid('example1.png', batches[:16], border_shade=0.8)
     h, w = batches.shape[2:]
-    crop_top, crop_bottom, crop_left, crop_right = 22, 29, 8, 0
+    crop_top, crop_bottom, crop_left, crop_right = 20, 30, 8, 0
+    print(batches.shape)
     batches = batches[:, :, crop_top:h-crop_bottom, crop_left:w-crop_right]
+    print(batches.shape)
     flat_batches = batches.transpose(1, 0, 2, 3).reshape(3, -1)
-    mean = flat_batches.mean(axis=1)[None, :, None, None]
-    std = flat_batches.std(axis=1)[None, :, None, None]
+    mean = flat_batches.mean(axis=1)[None, :, None, None] * 0.0  # FIXME multiplying 0
+    std = flat_batches.std(axis=1)[None, :, None, None] * 0.0 + 1.0  # FIXME setting to 1
     batches -= mean
     batches /= std
     # batches = batches.mean(axis=1, keepdims=True)  # greyscale
-    true_min = batches.min()
-    true_max = batches.max()
+    true_min = 0.0  # FIXME batches.min()
+    true_max = 1.0  # FIXME batches.max()
     print(true_min, true_max)
-    bmin = np.percentile(batches, 2)
-    bmax = np.percentile(batches, 98)
+    bmin = true_min  # np.percentile(batches, 2)
+    bmax = true_max  # np.percentile(batches, 98)
     print(bmin, bmax)
     batches = np.clip(batches, bmin, bmax)
-    batches -= batches.min()
-    batches /= batches.max()
+    batches -= bmin
+    batches /= bmax
     misc.save_comparison_grid('example2.png', batches[:16], border_shade=0.8)
 
     with open(DATA_DIR + '/img_stats.pk', 'wb') as f:
