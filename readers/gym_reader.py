cd2871233fc1f042698070df1e347e29e8dee8b1
Max Schwarzer <MaxA.Schwarzer@gmail.com>
2019-04-23 17:13:24 -0400

    Fix potential infinite loop in buffer initialization

--- readers/gym_reader.py
+++ readers/gym_reader.py
@@ -182,7 +182,7 @@ class ReplayBuffer(Reader):
             print('* Skipping replay buffer initialization')
         else:
             print('* Initializing replay buffer')
-            while self.buffer.count < initial_buffer_size:
+            while self.buffer.count < min(initial_buffer_size, buffer_size):
                 for conditional_batch in emulator.iter_batches('train', emulator.batch_size, threads=emulator.threads,
                                                                max_batches=int(np.ceil(buffer_size /
                                                                                        emulator.batch_size))):
